284f9797122f6a99875c240873eccacd8aa19a84
Arthur Moreira de Carvalho <126835082+Arthucs@users.noreply.github.com>
2025-04-01 15:15:56 -0300

    Update Ex8.java
    Adicionando o exercício 8

--- Ex8.java
+++ Ex8.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Ex8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int moto, carro, totalRodas, totalV;
+
+        System.out.println("Informe, respectivamente, o total de veiculos e o total de rodas:");
+        totalV = sc.nextInt();
+        totalRodas = sc.nextInt();
+
+        carro = ((totalV * -2) + totalRodas) / 2;
+        moto = totalV - carro;
         
+        System.out.println("Total de Carros: " + carro + "\nTotal de Motos: " + moto);
     }
 }
